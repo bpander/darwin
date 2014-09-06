720fe7b211620aac9b9a4f977faad3a4928e2708
Bradley Anderson <banderso@nerdery.com>
2014-09-06 17:11:05 -0500

    TravelingSalesmanApp now generates a seed population of Creatures and adds them to the Environment.

--- tests/traveling-salesman/TravelingSalesmanApp.js
+++ tests/traveling-salesman/TravelingSalesmanApp.js
@@ -2,6 +2,8 @@ define(function (require) {
     'use strict';
 
     var City = require('./City');
+    var RouteCreature = require('./RouteCreature');
+    var Environment = require('../../Environment');
 
 
     function TravelingSalesmanApp () {
@@ -10,18 +12,34 @@ define(function (require) {
 
         this.element = document.body;
 
+        this.environment = new Environment();
+
         this.init();
     }
 
 
     TravelingSalesmanApp.prototype.init = function () {
         this.generateRandomCities();
+        this.generatePopulation();
         return this;
     };
 
 
+    TravelingSalesmanApp.prototype.generatePopulation = function (populationSize) {
+        populationSize = typeof populationSize === 'number' ? Math.round(populationSize) : 5;
+        var i = 0;
+
+        this.environment.empty();
+
+        for (; i !== populationSize; i++) {
+            this.environment.push(new RouteCreature(this.cities));
+        }
+    };
+
+
     TravelingSalesmanApp.prototype.generateRandomCities = function (cityCount) {
-        cityCount = typeof cityCount === 'number' ? cityCount : 10;
+        cityCount = typeof cityCount === 'number' ? Math.round(cityCount) : 10;
+        this.cities = [];
         var city;
         var i = 0;
         for (; i !== cityCount; i++) {
