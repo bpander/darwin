acd784ebc9179f60d3f47b3af723b2745796ce6a
Bradley Anderson <banderso@nerdery.com>
2014-09-06 18:14:54 -0500

    Temporarily adding some hardcoded cities just for development.

--- tests/traveling-salesman/TravelingSalesmanApp.js
+++ tests/traveling-salesman/TravelingSalesmanApp.js
@@ -40,6 +40,26 @@ define(function (require) {
 
 
     TravelingSalesmanApp.prototype.generateRandomCities = function (cityCount) {
+        // TODO: Here's some hardcoded cities for testing
+        this.cities = [
+            new City(239.93783522280864, 33.83709011296742),
+            new City(594.1490982340183, 92.29226555675268),
+            new City(213.52676601754501, 81.95272134663537),
+            new City(592.9029894296546, 395.3669613294769),
+            new City(216.76033119834028, 555.4148496934213),
+            new City(291.54524080152623, 433.3915188363753),
+            new City(159.7737762457691, 307.4827005658299),
+            new City(422.5477536339313, 369.15487226634286),
+            new City(136.65842375345528, 117.29403634485789),
+            new City(412.0247841924429, 554.5568665754981)
+        ];
+        this.cities.forEach(function (city) {
+            this.element.appendChild(city.element);
+            city.render();
+        }, this);
+        return this;
+        // END TODO
+
         cityCount = typeof cityCount === 'number' ? Math.round(cityCount) : 10;
         this.cities = [];
         var city;
